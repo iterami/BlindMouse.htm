20c4fa01b54a9bbada7c5f688b6f096a214a3f97
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 12:45:54 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,36 @@
 'use strict';
 
+function gameover(){
+    running = false;
+    core_elements['canvas'].style.cursor = 'pointer';
+}
+
+function load_data(id){
+    core_elements['canvas'].style.cursor = 'none';
+    click_x = -1;
+    click_y = -1;
+    score = 0;
+
+    randomize_shapes();
+    running = true;
+}
+
+function randomize_shapes(){
+    entity_remove_all();
+
+    entity_create({
+      'id': 'target',
+      'properties': {
+        'x': core_random_integer({
+          'max': canvas_properties['width'] - core_storage_data['target-width'],
+        }),
+        'y': core_random_integer({
+          'max': canvas_properties['height'] - core_storage_data['target-height'],
+        }),
+      },
+    });
+}
+
 function repo_drawlogic(){
     if(!entity_entities['target']){
         return;
