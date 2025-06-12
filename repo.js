714e95a480c211c19a373ce71044e47c9fff3465
Jan Hořava <czechinthemail@gmail.com>
2025-06-12 11:17:01 -0700

    updated core_random_integer() usage

--- repo.js
+++ repo.js
@@ -19,12 +19,8 @@ function randomize_shapes(){
     entity_create({
       'id': 'target',
       'properties': {
-        'x': core_random_integer({
-          'max': canvas_properties['width'] - core_storage_data['target-width'],
-        }),
-        'y': core_random_integer({
-          'max': canvas_properties['height'] - core_storage_data['target-height'],
-        }),
+        'x': core_random_integer(canvas_properties['width'] - core_storage_data['target-width']),
+        'y': core_random_integer(canvas_properties['height'] - core_storage_data['target-height']),
       },
     });
 
